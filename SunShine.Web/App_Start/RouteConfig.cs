c365f60fda34175f45157c0e1d9334db141d5a2d
yangbinqi <ybq@outlook.com>
2017-02-28 23:26:20 +0800

    Friend URL

--- SunShine.Web/App_Start/RouteConfig.cs
+++ SunShine.Web/App_Start/RouteConfig.cs
@@ -10,11 +10,35 @@ namespace SunShine.Web {
         public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+           // routes.MapRoute(
+           //    name: "product",
+           //    url: "Product/List_{idproduct}_{idcategory}_{categoryCode}_{keyword}_{pageIndex}.html",
+           //    defaults: new
+           //    {
+           //        controller = "Product",
+           //        action = "List",
+           //        idproduct = UrlParameter.Optional,
+           //        idcategory = UrlParameter.Optional,
+           //        categoryCode = UrlParameter.Optional,
+           //        keyword = UrlParameter.Optional,
+           //        pageIndex = UrlParameter.Optional
+           //    }
+           //);
+
+            routes.MapRoute(
+                name: "Default_html",
+                url: "{controller}/{action}.html",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}",
                 defaults: new { controller = "Home", action = "Index" }
             );
+
+           
+
         }
     }
 }
